217eb2a64149a64f1b32ba7067d77aa37d1c50db
yetanotherqubick <116007494+yetanotherqubick@users.noreply.github.com>
2026-09-02 17:53:41 +0300

    Remove unused QSPTools helpers

--- src/application/comtools.h
+++ src/application/comtools.h
@@ -9,10 +9,8 @@
 class QSPTools
 {
 public:
-    static QString GetHexColor(const QColor &color);
     static QString HtmlizeWhitespaces(const QString &str);
     static QString ProceedAsPlain(const QString &str);
-    static QString GetAppPath();
     static QString GetCaseInsensitiveFilePath(QString searchDir, QString originalPath);
     static QString GetCaseInsensitiveAbsoluteFilePath(QString searchDir, QString originalPath);
     static QString qspStrToQt(const QSP_CHAR *str);
